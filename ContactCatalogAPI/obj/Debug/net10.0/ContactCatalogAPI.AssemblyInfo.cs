b9204ebbba767b7b8e487758dd00ccc0b5832996
Hande Bengü <hande.bengu@chasacademy.se>
2026-02-23 00:28:52 +0100

    add frontend - index.html, style.css, app.js

--- ContactCatalogAPI/obj/Debug/net10.0/ContactCatalogAPI.AssemblyInfo.cs
+++ ContactCatalogAPI/obj/Debug/net10.0/ContactCatalogAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactCatalogAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22487d19143e87c0ebacd0e10ae85d07b4ef1687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76ac1cb2c648238ff62f8115657209adf6c00f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactCatalogAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactCatalogAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
